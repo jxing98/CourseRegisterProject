d6a90ec06199d94f833caa36b553ccf5f044039c
jxing98 <jxing@umass.edu>
2022-10-28 21:40:39 -0400

    Delete old sql file

--- Databasefiles/users.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `users` (`id`, `email`, `name`, `createdAt`, `updatedAt`) VALUES (1, 'aaa@qq.com', 'aaa', '2022-10-28 20:35:09', '2022-10-28 20:35:09');
-INSERT INTO `users` (`id`, `email`, `name`, `createdAt`, `updatedAt`) VALUES (2, 'bbb@qq.com', 'bbb', '2022-10-28 20:35:09', '2022-10-28 20:35:09');
-INSERT INTO `users` (`id`, `email`, `name`, `createdAt`, `updatedAt`) VALUES (3, 'bbbb@qq.com', 'bbb', '2022-10-28 20:35:09', '2022-10-28 20:35:09');
