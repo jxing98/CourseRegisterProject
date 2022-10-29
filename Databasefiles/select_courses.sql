78cf1cd92e2d427bd2886cb0c954c25958baef78
jxing98 <jxing@umass.edu>
2022-10-28 20:51:32 -0400

    Upload database file

---
+++ Databasefiles/select_courses.sql
@@ -0,0 +1,3 @@
+INSERT INTO `select_courses` (`id`, `courseId`, `studentId`, `comment`, `createdAt`, `updatedAt`) VALUES (1, '1', '1', '', '2022-10-28 20:35:01', '2022-10-28 20:35:01');
+INSERT INTO `select_courses` (`id`, `courseId`, `studentId`, `comment`, `createdAt`, `updatedAt`) VALUES (2, '1', '2', '', '2022-10-28 20:35:01', '2022-10-28 20:35:01');
+INSERT INTO `select_courses` (`id`, `courseId`, `studentId`, `comment`, `createdAt`, `updatedAt`) VALUES (3, '1', '3', '', '2022-10-28 20:35:01', '2022-10-28 20:35:01');
